10f2bc0cd4961809386ccd15a8b4e7895adc512a
Ruben Daniels <ruben@c9.io>
2014-01-27 12:19:46 +0100

    Big rename from 'Cloud9 IDE' to 'Cloud9'

--- closeconfirmation.js
+++ closeconfirmation.js
@@ -65,7 +65,7 @@ define(function(require, exports, module) {
                 if (changed)
                     return "You have unsaved changes. Your changes will be lost if you don't save them";
                 else
-                    return "You're about to leave Cloud9 IDE.";
+                    return "You're about to leave Cloud9.";
             }
         }
 
